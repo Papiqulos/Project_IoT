9db063e4a62f34185b61cab4f124d126b789609b
George Papoutsas <osfpgeopap1925@gmail.com>
2025-02-08 11:06:11 +0200

    organised code

--- public/js/map.js
+++ public/js/map.js
@@ -4,13 +4,48 @@ let suggestionsClicked = false;
 let heatmapsClicked = false;
 let isDragging = false;
 const center_HMTY = [38.28806669351595, 21.78915113469408];
-let center1 = { lat: center_HMTY[0], lng: center_HMTY[1] };
+let default_center = { lat: center_HMTY[0], lng: center_HMTY[1] };
 const googleApiKey = getGoogleApiKey();
 let selectedOrigin;
 let selectedDestination;
 let currentLocation;
 let currentDirections;
 
+// USER LOCATION
+// Get the user's current location and store it in a global variable
+function getCurrentLocation(callback) {
+  infoWindow = new google.maps.InfoWindow();
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const pos = 
+        {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        window.currentLocation = pos;
+        callback();
+      }
+      ,
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      },
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+    return 0;
+  }
+}
+
+function toggleCurrentLocation() {
+  infoWindow = new google.maps.InfoWindow();
+  infoWindow.setPosition(window.currentLocation);
+  infoWindow.setContent("Location found.");
+  infoWindow.open(map);
+  map.setCenter(window.currentLocation);
+}
+
+// LIMIT API REQUESTS
 // Debounce function to limit API requests
 function debounce(func, wait) {
   let timeout;
@@ -20,29 +55,7 @@ function debounce(func, wait) {
   };
 }
 
-async function showDirections() {
-  console.log("searching place");
-    const travelModeRadios = document.getElementsByName('travel_mode');
-    let selectedTravelMode;
-    let selectedOrigin = window.selectedOrigin || window.currentLocation;
-    let selectedDestination = window.selectedDestination || {lat : 38.23666252117088, lng: 21.732572423403976}; // defaults to euagellobill
-
-    for (const radio of travelModeRadios) {
-      if (radio.checked) {
-        selectedTravelMode = radio.value;
-        break;
-      }
-    }
-
-    // Get directions to the place
-    // console.log("Origin", selectedOrigin);
-    // console.log("Destination", selectedDestination);
-    // console.log("selectedTravelMode", selectedTravelMode);
-    clearDirections();
-    clearMarkers();
-    getDirections(selectedOrigin, selectedDestination, selectedTravelMode);
-}
-
+// AUTOCOMPLETE SEARCH BAR
 async function autoCompleteOriginDestination() {
   // console.log("searching place");
 
@@ -53,7 +66,7 @@ async function autoCompleteOriginDestination() {
   const originInput = document.getElementById('origin_input').value;
 
   // Find the origins based on the user input
-  let selectedOrigins = await findPlacesByText(originInput, 5) || [];
+  let selectedOrigins = await textSearchPlace(originInput, 5) || [];
 
   // Add the results below the input
   selectedOrigins.forEach((place, index) => {
@@ -74,7 +87,7 @@ async function autoCompleteOriginDestination() {
   // console.log("user_input", userInput);
 
   // Find the destinations based on the user input
-  let selectedDestinations = await findPlacesByText(destinationInput, 5) || [];
+  let selectedDestinations = await textSearchPlace(destinationInput, 5) || [];
 
   // Add the results below the input
   selectedDestinations.forEach((place, index) => {
@@ -92,60 +105,7 @@ async function autoCompleteOriginDestination() {
 
 }
 
-async function toggleAirQuality() {
-  try {
-
-    const airQualityData = await getAirQualityData(window.currentLocation);
-    // Create a container for the air quality data
-    const airQualityContainer = document.createElement("div");
-    airQualityContainer.classList.add("air-quality-container");
-
-    // Populate the container with air quality data
-    airQualityContainer.innerHTML = `
-      <div class="notifications">
-
-        <div class="notification">
-      Date and Time: ${airQualityData.dateTime}
-        </div>
-
-        <div class="notification">
-      Region Code: ${airQualityData.regionCode}
-        </div>
-
-        <div class="notifications" id="notifications">
-      ${airQualityData.indexes.map(index => `
-        
-        <div class="notification">
-          ${Object.entries(index).map(([key, value]) => `
-        <div>
-          ${key === 'color' ? Object.entries(value).map(([colorKey, colorValue]) => `
-            <div>${colorKey}: ${colorValue}</div>
-          `).join('') : `${key}: ${value}`}
-        </div>
-          `).join('')}
-        </div>
-      `).join('')}
-        </div>
-      </div>
-    `;
-    
-    //Append the container to the map if it doesn't exist
-    if (!document.querySelector(".air-quality-container")) {
-      console.log("appending airQualityContainer");
-      map.controls[google.maps.ControlPosition.RIGHT].push(airQualityContainer);
-    } else {
-      console.log("removing airQualityContainer");
-      // Remove the container if it exists
-      map.controls[google.maps.ControlPosition.RIGHT].pop();
-    }
-    
-    
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
-
+// HEATMAPS
 function toggleHeatmap() {
 
   // // Hide the greeting text if it is visible
@@ -170,46 +130,10 @@ function toggleHeatmap() {
   heatmap.setMap(heatmap.getMap() ? null : map);
 }
 
-function toggleSuggestions(){
-
-  // Hide the greeting text if it is visible
-  // var greetingText = document.getElementById('greeting-container');
-  // if (greetingText.style.display === 'block') {
-  //   greetingText.style.display = 'none';
-  // }
-
-  // // Hide the heatmaps form if it is visible
-  // var heatmapsForm = document.getElementById('heatmapsForm');
-  // if (heatmapsForm.style.display === 'block') {
-  //   heatmapsForm.style.display = 'none';
-  // }
-
-  // //Hide the heatmap if it is visible
-  // // clearHeatmap();
-  
-  // var suggestionsForm = document.getElementById('suggestionsForm');
-
-  // if (suggestionsForm.style.display === 'none') {
-  //   suggestionsForm.style.display = 'flex';
-  // } else {
-  //   suggestionsForm.style.display = 'none';
-  // }
-
-  suggestionsClicked = !suggestionsClicked;
-  heatmapsClicked = false;
-  clearMarkers();
-
-  // Debugging
-  console.log("suggestionsClicked", suggestionsClicked);
-  console.log("heatmapsClicked", heatmapsClicked);
-}
-
-function toggleCurrentLocation() {
-  infoWindow = new google.maps.InfoWindow();
-  infoWindow.setPosition(window.currentLocation);
-  infoWindow.setContent("Location found.");
-  infoWindow.open(map);
-  map.setCenter(window.currentLocation);
+function clearHeatmap() {
+  if (heatmap) {
+    heatmap.setMap(null);
+  }
 }
 
 function changeGradient() {
@@ -241,6 +165,7 @@ function changeOpacity() {
   heatmap.set("opacity", heatmap.get("opacity") ? null : 0.2);
 }
 
+// Get the points for the heatmap
 // Heatmap data: 500 Points
 function getPoints() {
   return [
@@ -267,62 +192,6 @@ async function getGoogleApiKey() {
   return data.key; 
 }
 
-// Finds nearby places and displays heatmap data for them (for the moment only markers appear on nearby places) 
-async function nearbySearch(results = 5) {
-    //@ts-ignore
-    const { Place, SearchNearbyRankPreference } = await google.maps.importLibrary(
-      "places",
-    );
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    // Restrict within the map viewport.
-    let center = new google.maps.LatLng(center1.lat, center1.lng);
-    const request = {
-      // required parameters
-      fields: ["displayName", "location", "businessStatus", "userRatingCount"],
-      locationRestriction: {
-        center: center,
-        radius: 700,
-      },
-      // optional parameters
-      maxResultCount: results,
-      rankPreference: SearchNearbyRankPreference.POPULARITY,
-      language: "en-GR",
-      region: "gr",
-    };
-    //@ts-ignore
-    const { places } = await Place.searchNearby(request);
-  
-    if (places.length) {
-    //   console.log(places);
-      
-      // New bounds to fit all the markers
-      const { LatLngBounds } = await google.maps.importLibrary("core");
-      const bounds = new LatLngBounds();
-
-      // Clear existing markers (optional)
-      clearMarkers();
-  
-      // Loop through and get all the results.
-      places.forEach((place) => {
-        const markerView = new AdvancedMarkerElement({
-          map,
-          position: place.location,
-          title: place.displayName,
-        });
-  
-        bounds.extend(place.location);
-        console.log(place.displayName);
-
-        // Store the marker for later removal
-        window.markers.push(markerView);
-      });
-      // Fit the map to the bounds
-      map.fitBounds(bounds); 
-    } else {
-      console.log("No results");
-    }
-}
-
 function clearMarkers() {
   // Clear existing markers if any
   if (window.markers) {
@@ -331,8 +200,38 @@ function clearMarkers() {
   window.markers = [];
 }
 
-// Find a place by text 
-async function findPlacesByText(text, results = 15) {
+// VARIOUS SEARCHES
+// Finds nearby places based on a given center
+async function nearbySearchPlace(center = default_center, results = 5) {
+  const { Place, SearchNearbyRankPreference } = await google.maps.importLibrary(
+    "places",
+  );
+
+  const request = {
+    // required parameters
+    fields: ["displayName", "location", "businessStatus", "userRatingCount"],
+    locationRestriction: {
+      center: center,
+      radius: 700,
+    },
+    // optional parameters
+    maxResultCount: results,
+    rankPreference: SearchNearbyRankPreference.POPULARITY,
+    language: "en-GR",
+    region: "gr",
+  };
+  const { places } = await Place.searchNearby(request);
+
+  if (places.length) {
+    console.log("Found results");
+    return places;
+  } else {
+    console.log("No results");
+  }
+}
+
+// Perform a text-based search for places
+async function textSearchPlace(text, results = 15) {
 
     if (!text) {
       console.log("No text provided");
@@ -374,7 +273,8 @@ async function findPlacesByText(text, results = 15) {
   }
 }
 
-// Get directions from the center of the map to a specific location
+// DIRECTONS
+// Get directions and intermediate markers from an origin to a destination with a specific travel mode 
 async function getDirections(origin, destination, travelMode = "DRIVING") {
   // Clear any existing directions
   clearDirections();
@@ -392,47 +292,93 @@ async function getDirections(origin, destination, travelMode = "DRIVING") {
     travelMode: travelMode,
   
   };
-  directionsService.route(request, async function(result, status) {
-    if (status == 'OK') {
-      directionsRenderer.setDirections(result);
-      // console.log("result", JSON.stringify(result.routes[0].legs[0].steps[0].path));
-      
-      const steps = result.routes[0].legs[0].steps;
-      steps.forEach((step) => {
-        const path = step.path;
-        const middlePoint = path[Math.floor(path.length / 2)];
-        // Visualize the middle point of each step
-        const markerView = new google.maps.marker.AdvancedMarkerElement({
-          position: middlePoint,
-          map: map,
-          title: step.instructions,
-        });
-        // Store the marker for later removal
-        window.markers.push(markerView);
-      });
-
+  directionsService
+    .route(request, 
+    async function(result, status) {
+      if (status == 'OK') {
+        directionsRenderer.setDirections(result);
+        // console.log("result", JSON.stringify(result.routes[0].legs[0].steps[0].path));
+        
+        try {
+          const steps = result.routes[0].legs[0].steps;
+          if (steps) {
+            steps.forEach(async (step, i) => {
+              const path = step.path;
+              const middlePoint = path[Math.floor(path.length / 2)];
+              console.log("middlePoint", middlePoint);
+              // Get the air quality data for the middle point of each step
+              // const airQualityData = await getAirQualityData(middlePoint);
+              const markerTitle = "Air Quality Data";
+              const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+                "marker",
+              );
+              const pin = new PinElement({
+                glyph: `${i + 1}`,
+                scale: 1.5,
+              });
+              // Visualize the middle point of each step
+              const markerView = new AdvancedMarkerElement({
+                position: middlePoint,
+                map: map,
+                title: markerTitle,
+                content: pin.element,
+                gmpClickable: true,
+              });
+              markerView.addListener("click", ({docEvent, latLng}) => 
+                {
+                  infoWindow.close();
+                  infoWindow.setContent(markerView.title);
+                  infoWindow.open(markerView.map, markerView);
+                });
+              // Store the marker for later removal
+              window.markers.push(markerView);
+            });
+          }
+          else {
+            console.log("No steps found");
+          }
+        }
+        catch (error) {console.error("Error getting intermediate markers:", error);};
       }
-    });
-  }
+    }
+  );
+}
+
+// Visualize the directions and the intermediate markers on the map
+async function showDirections() {
+  console.log("searching place");
+    const travelModeRadios = document.getElementsByName('travel_mode');
+    let selectedTravelMode;
+    let selectedOrigin = window.selectedOrigin || window.currentLocation;
+    let selectedDestination = window.selectedDestination || {lat : 38.23666252117088, lng: 21.732572423403976}; // defaults to euagellobill
+
+    for (const radio of travelModeRadios) {
+      if (radio.checked) {
+        selectedTravelMode = radio.value;
+        break;
+      }
+    }
+
+    // Get directions to the place
+    // console.log("Origin", selectedOrigin);
+    // console.log("Destination", selectedDestination);
+    // console.log("selectedTravelMode", selectedTravelMode);
+    clearDirections();
+    clearMarkers();
+    getDirections(selectedOrigin, selectedDestination, selectedTravelMode);
+}
 
 // Function to clear directions
 function clearDirections() {
-  if (window.currentDirections) {
-    window.currentDirections.setMap(null);
-    window.currentDirections = null;
+  // Remove any directions visible on the map
+  if (currentDirections) {
+    currentDirections.setMap(null);
   }
 }
 
-function clearHeatmap() {
-  if (heatmap) {
-    heatmap.setMap(null);
-  }
-}
-
+// AIR QUALITY DATA
 // Function to get Air Quality Data for a specific location
 async function getAirQualityData(location_) {
-  const lat = location_.lat;
-  const lng = location_.lng;
 
   let googleApiKeyResolved = await googleApiKey;
 
@@ -441,8 +387,8 @@ async function getAirQualityData(location_) {
       `https://airquality.googleapis.com/v1/currentConditions:lookup?key=${googleApiKeyResolved}`,
       {
         location: {
-          latitude: lat, // Latitude from the location
-          longitude: lng, // Longitude from the location
+          latitude: location_.lat,
+          longitude: location_.lng,
         },
       //   extraComputations: ['HEALTH_RECOMMENDATIONS'], // Optional: Add extra computations
         languageCode: 'en', // Optional: Set the language
@@ -464,30 +410,70 @@ async function getAirQualityData(location_) {
 
 }
 
-// Get the user's current location and store it in a global variable
-function getCurrentLocation(callback) {
-  infoWindow = new google.maps.InfoWindow();
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-        const pos = 
-        {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        window.currentLocation = pos;
-        callback();
-      }
-      ,
-      () => {
-        handleLocationError(true, infoWindow, map.getCenter());
-      },
-    );
-  } else {
-    // Browser doesn't support Geolocation
-    handleLocationError(false, infoWindow, map.getCenter());
-    return 0;
+// NEEDS CHANGES
+// Show the air quality information based on the user's location and his selected route
+async function toggleAirQuality() {
+  try {
+
+    //Append the container to the map if it doesn't exist
+    if (!document.querySelector(".air-quality-container")) {
+      console.log("appending airQualityContainer");
+
+
+      const airQualityData = await getAirQualityData(window.currentLocation);
+      // Create a container for the air quality data
+      const airQualityContainer = document.createElement("div");
+      airQualityContainer.classList.add("air-quality-container");
+
+      // Populate the container with air quality data
+      airQualityContainer.innerHTML = `
+        <div class="notifications">
+
+          <div class="notification">
+        Date and Time: ${airQualityData.dateTime}
+          </div>
+
+          <div class="notification">
+        Region Code: ${airQualityData.regionCode}
+          </div>
+
+          <div class="notifications" id="notifications">
+        ${airQualityData.indexes.map(index => `
+          
+          <div class="notification">
+            ${Object.entries(index).map(([key, value]) => `
+          <div>
+            ${key === 'color' ? Object.entries(value).map(([colorKey, colorValue]) => `
+              <div>${colorKey}: ${colorValue}</div>
+            `).join('') : `${key}: ${value}`}
+          </div>
+            `).join('')}
+          </div>
+        `).join('')}
+          </div>
+        </div>
+      `;
+
+
+
+      map.controls[google.maps.ControlPosition.RIGHT].push(airQualityContainer);
+    } else {
+      console.log("removing airQualityContainer");
+      // Remove the container if it exists
+      map.controls[google.maps.ControlPosition.RIGHT].pop();
+    }
+
+    
+    
+    
+    
+    
+  }
+  catch (error) {
+    console.error(error);
   }
 }
+
 
 // Initialize the map
 async function initMap() {
@@ -495,7 +481,7 @@ async function initMap() {
   //// Initialize the map
   const { Map } = await google.maps.importLibrary("maps");
   map = new Map(document.getElementById("map"), {
-    center: center1, // HMTY 38.26469392470636, 21.742012983437373
+    center: default_center, // HMTY 38.26469392470636, 21.742012983437373
     zoom: 17,
     mapId: "DEMO_MAP_ID",
     disableDefaultUI: true,
@@ -522,7 +508,7 @@ async function initMap() {
   
   // Debounced version of nearbySearch
   const debouncedNearbySearch = debounce(() => {
-    nearbySearch();
+    nearbySearchPlace();
   }, 500); // Adjust the delay as needed (e.g., 1000ms)
 
   /////// MAP CONTROLS
@@ -547,7 +533,8 @@ async function initMap() {
     results.style.display = "flex";
     results.style.flexDirection = "row";
     // Get the nearby places accourding to the user's location
-    // nearbySearch();
+    const nearbyOrigins = await nearbySearchPlace();
+    nearbyOrigins.forEach((place, index) => {console.log("place", place.displayName);})
   });
 
   //When the search bar is unfocused
@@ -615,13 +602,6 @@ async function initMap() {
       }
   });
 
-
-
-  // Listener for the "SEARCH PLACES" button
-  // Get a list of results from a place search and store them for later use
-  // document
-  //   .getElementById('search_place')
-  //   .addEventListener('click', autoCompleteOriginDestination);
 
   // Listener for the "GET DIRECTIONS" button
   // Get directions to a place
